8a91698f76df2a770feb4acf8a93f2aac3d70580
Jared <Jared@Littles-MacBook-Pro.local>
2018-02-20 15:53:27 -0500

    posting to heroku asap

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,30 @@
+// Set up MySQL connection.
+var mysql = require("mysql");
 
-var mysql = require('mysql');
+var connection;
+if (process.env.JAWSDB_URL) {
+  //Heroku deployment
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  //local host
+  connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "burgers_db"
+  });
+};
+// var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+//   port: 3306,
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "burgers_db"
+// });
 
 // Make connection.
 connection.connect(function(err) {
